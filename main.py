23d02a3e249c610d3f522c3937aa02210022cbff
mokogawa <s1091035@lmjh.tp.edu.tw>
2021-05-06 20:05:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print('==Bot is online==')
 
-bot.run("ODM5ODIxMTA0NTI3ODM1MTg2.YJPOWg.nf_nve29_Hl1ES4vT35y5Hq_4eY")
+bot.run("<token>")
